fe226c24f76f720e9c5dc470944cc30cfaa79c8b
sreejithb14 <sreejith.b@seqato.com>
2018-12-26 17:41:42 +0530

    dgds

--- src/main/java/exception/Exceptions.java
+++ src/main/java/exception/Exceptions.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package exception;
 
 import java.io.IOException;
@@ -22,28 +21,3 @@ public class Exceptions {
 		}
 	}
 }
-=======
-package exception;
-
-import java.io.IOException;
-
-public class Exceptions {
-	public  void foo() throws Throwable   {
-			int a =5/0;
-			throw new  Exception();
-		
-	}
-	public static void main(String args[])  /*throws Exception*/  {
-		try{
-
-			System.out.println("hello.........");
-			new Exceptions().foo();
-			new Exceptions().foo();
-			System.out.println("hello");
-		}catch(Throwable e) {
-			e.printStackTrace();
-			System.out.println("conflict testing...");
-		}
-	}
-}
->>>>>>> 31731d940546b847c9265055267dca9d99110c3c
