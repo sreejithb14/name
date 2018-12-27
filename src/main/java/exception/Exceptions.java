be37786ec2168dc70d256511cd2c29d99b5b2c69
sreejithb14 <sreejith.b@seqato.com>
2018-12-27 10:37:54 +0530

    sa

--- src/main/java/exception/Exceptions.java
+++ src/main/java/exception/Exceptions.java
@@ -12,20 +12,12 @@ public class Exceptions {
 	public static void main(String args[])  /*throws Exception*/  {
 		try{
 
-			System.out.println("hello....sreejithee.....how are you  ");
 			new Exceptions().foo();
-			new Exceptions().foo();
-			System.out.println("hello sreejith how do you do");
+
 		}catch(Throwable e) {
 			e.printStackTrace();
 			System.out.println("conflict testing..123....i am going to make a conflict..");
-			System.out.println("conflict testing..123....");
-			System.out.println("hii");	
-			System.out.println("hii");
-			System.out.println("hii");
-			System.out.println("conflict testing...");
-			System.out.println("conflict testing...");
-			System.out.println("conflict testing..123.");
+
 
 		}
 	}
