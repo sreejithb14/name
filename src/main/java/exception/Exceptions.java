85a456a98b4b6a9b918d1f51f937e7e152b28f15
sreejithb14 <sreejith.b@seqato.com>
2018-12-27 11:23:49 +0530

    conflict

--- src/main/java/exception/Exceptions.java
+++ src/main/java/exception/Exceptions.java
@@ -7,21 +7,13 @@ public class Exceptions {
 	public  void foo() throws Throwable   {
 			int a =5/0;
 			throw new  Exception();
-		
 	}
 	public static void main(String args[])  /*throws Exception*/  {
 		try{
-
 			new Exceptions().foo();
-
 		}catch(Throwable e) {
 			e.printStackTrace();
-			System.out.println("conflict testing..123....i am going to make a conflict..123");
-
-			
+			System.out.println("conflict testing..123....i am going to make a conflict..123");			
 		}
 	}
 }
-
-
-
