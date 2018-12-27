4dacb6472ba953cfbe3e7f5c7cfc0168b575ccb1
chinchuantonys <chinchu.as@seqato.com>
2018-12-27 10:57:52 +0530

    1

--- src/main/java/exception/Exceptions.java
+++ src/main/java/exception/Exceptions.java
@@ -17,6 +17,28 @@ public class Exceptions {
 		}catch(Throwable e) {
 			e.printStackTrace();
 			System.out.println("conflict testing..123....i am going to make a conflict..");
+<<<<<<< c24918be7850591e9f9facdae7e9e4072477659b
+||||||| merged common ancestors
+
+
+			System.out.println("conflict testing...");
+			System.out.println("conflict testing...");
+			System.out.println("conflict testing..123.");
+			System.out.println("hello");
+
+	
+
+=======
+
+
+			System.out.println("conflict testing...");
+			System.out.println("conflict testing...");
+			System.out.println("conflict testing..123.");
+			System.out.println("hello1");
+
+	
+
+>>>>>>> 1
 		}
 	}
 }
